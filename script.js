5b0f8ae2a64ff540a8e6e52f5552e62319698182
Tej370 <153205267+Tej370@users.noreply.github.com>
2024-07-03 23:12:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function firstChar(text) {
   // your code here
+	    for (let i = 0; i < text.length; i++) {
+        // Check if the character is not a space
+        if (text[i] !== ' ') {
+            // Return the first non-space character
+            return text[i];
+        }
+    }
+    // If only spaces are found or the string is empty, return an empty string
+    return '';
 }
+
+
 
 // Do not change the code below
 
